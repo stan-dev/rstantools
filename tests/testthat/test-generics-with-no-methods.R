336ca560447077fb82595dfaf94d3b8bbdf6bace
jgabry <jgabry@gmail.com>
2016-10-30 23:45:56 -0400

    update tests

--- tests/testthat/test-generics-with-no-methods.R
+++ tests/testthat/test-generics-with-no-methods.R
@@ -1,4 +1,4 @@
-context("default methods")
+context("generics with no methods")
 set.seed(1111)
 x <- matrix(rnorm(150), 50, 3)
 y <- rnorm(ncol(x))
@@ -7,6 +7,8 @@ y <- rnorm(ncol(x))
 test_that("generics without defaults ok", {
   expect_true(exists("log_lik", mode = "function"))
   expect_true(exists("posterior_predict", mode = "function"))
+  expect_error(log_lik(2), "no applicable method")
+  expect_error(posterior_predict(2), "no applicable method")
 
   expect_output(print(methods("log_lik")), "no methods found")
   expect_output(print(methods("posterior_predict")), "no methods found")
