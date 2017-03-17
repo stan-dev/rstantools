5c1fca9c366b4887a907fde96f0a2dfa1a20e07a
jgabry <jgabry@gmail.com>
2017-03-17 12:53:23 -0400

    reduce code copying in tests

--- tests/testthat/test-generics-with-no-methods.R
+++ tests/testthat/test-generics-with-no-methods.R
@@ -4,21 +4,20 @@ x <- matrix(rnorm(150), 50, 3)
 y <- rnorm(ncol(x))
 
 
-test_that("generics without defaults ok", {
-  expect_true(exists("log_lik", mode = "function"))
-  expect_true(exists("posterior_predict", mode = "function"))
-  expect_true(exists("loo_predict", mode = "function"))
-  expect_true(exists("loo_linpred", mode = "function"))
-  expect_true(exists("loo_predictive_interval", mode = "function"))
-  expect_error(log_lik(2), "no applicable method")
-  expect_error(posterior_predict(2), "no applicable method")
-  expect_error(loo_predict(2), "no applicable method")
-  expect_error(loo_linpred(2), "no applicable method")
-  expect_error(loo_predictive_interval(2), "no applicable method")
+generics_w_no_default <-
+  c(
+    "log_lik",
+    "posterior_predict",
+    "posterior_linpred",
+    "loo_predict",
+    "loo_linpred",
+    "loo_predictive_interval"
+  )
 
-  expect_output(print(methods("log_lik")), "no methods found")
-  expect_output(print(methods("posterior_predict")), "no methods found")
-  expect_output(print(methods("loo_linpred")), "no methods found")
-  expect_output(print(methods("loo_predict")), "no methods found")
-  expect_output(print(methods("loo_predictive_interval")), "no methods found")
+test_that("generics without defaults ok", {
+  for (f in generics_w_no_default) {
+    expect_true(exists(f, mode = "function"), info = f)
+    expect_error(get(f, mode = "function")(2), "no applicable method", info = f)
+    expect_output(print(methods(f)), "no methods found", info = f)
+  }
 })
