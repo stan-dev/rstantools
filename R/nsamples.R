6fa5a0f4b0d4a980331b14846e33a646331d8429
jgabry <jgabry@gmail.com>
2023-03-09 17:44:51 -0700

    deprecate nsamples

--- R/nsamples.R
+++ R/nsamples.R
@@ -1,11 +1,11 @@
-#' Generic function for extracting the number of posterior samples
-#'
 #' Extract the number of posterior samples stored in a fitted Bayesian model.
 #'
 #' @export
+#' @keywords internal
 #' @template args-object
 #' @template args-dots
 #'
 nsamples <- function(object, ...) {
+  .Deprecated("ndraws from the posterior package")
   UseMethod("nsamples")
 }
