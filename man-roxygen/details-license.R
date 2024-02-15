46b08c9cad9a88b1f9fd0a226eaebe06794d0335
Teemu Säilynoja <teemu.sailynoja@gmail.com>
2024-02-15 14:14:09 +0200

    Removed a bug where a line starting with > was parsed as a block quote.

--- man-roxygen/details-license.R
+++ man-roxygen/details-license.R
@@ -1,6 +1,6 @@
 #' @details In order to enable Stan functionality, \pkg{\link{rstantools}}
-#'   copies some files to your package.  Since these files are licensed as GPL
-#'   >= 3, the same license applies to your package should you choose to
+#'   copies some files to your package.  Since these files are licensed as 
+#'   GPL >= 3, the same license applies to your package should you choose to
 #'   distribute it.  Even if you don't use \pkg{\link{rstantools}} to create
 #'   your package, it is likely that you will be linking to \pkg{\link{Rcpp}} to
 #'   export the Stan C++ `stanmodel` objects to \R.  Since
