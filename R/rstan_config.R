681262c2beaf8e44251d3c3c06ff332e3f77d767
Marco Colombo <mar.colombo13@gmail.com>
2025-06-04 09:54:04 +0200

    Add fallback package name in case the DESCRIPTION file cannot be read.
    When the DESCRIPTION file is not present, the package name is inferred to
    be the basename of pkgdir, which is what it used to be before #130.

--- R/rstan_config.R
+++ R/rstan_config.R
@@ -214,7 +214,10 @@ rstan_config <- function(pkgdir = ".") {
     eigen_incl <- ifelse(utils::packageVersion('StanHeaders') >= "2.31",
                          "#include <stan/math/prim/fun/Eigen.hpp>",
                          "#include <stan/math/prim/mat/fun/Eigen.hpp>")
-    pkgname <- unname(read.dcf("DESCRIPTION", "Package")[1, 1])
+    pkgname <- tryCatch(unname(read.dcf("DESCRIPTION", "Package")[1, 1]),
+                        warning = function(w) {
+                          basename(pkgdir)
+                        })
     cat("#include <exporter.h>",
         eigen_incl,
         "#include <stan/math/prim/meta.hpp>",
