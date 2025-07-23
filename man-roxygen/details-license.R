1c8785389257af8c902d331268fe19280657dc88
jgabry <jgabry@gmail.com>
2025-07-23 10:55:49 -0600

    fix R cmd check NOTE and regenerate doc

--- man-roxygen/details-license.R
+++ man-roxygen/details-license.R
@@ -1,8 +1,8 @@
 #' @details In order to enable Stan functionality, \pkg{\link{rstantools}}
-#'   copies some files to your package.  Since these files are licensed as 
+#'   copies some files to your package.  Since these files are licensed as
 #'   GPL >= 3, the same license applies to your package should you choose to
 #'   distribute it.  Even if you don't use \pkg{\link{rstantools}} to create
-#'   your package, it is likely that you will be linking to \pkg{\link{Rcpp}} to
+#'   your package, it is likely that you will be linking to \pkg{Rcpp} to
 #'   export the Stan C++ `stanmodel` objects to \R.  Since
-#'   \pkg{\link{Rcpp}} is released under GPL >= 2, the same license would apply
+#'   \pkg{Rcpp} is released under GPL >= 2, the same license would apply
 #'   to your package upon distribution.
